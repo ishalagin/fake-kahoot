8fe0ee9cae22422beae522f9fbb0f18492839e1c
p3dr4m <seenkim@gmail.com>
2018-05-12 20:51:45 -0700

    editted users.test.js

--- tests/users.test.js
+++ tests/users.test.js
@@ -1,28 +1,48 @@
-const usersM = require('../models/users');
+/* eslint-env jest */
+const usersM = require('../models/users')
+
+beforeAll(() => {
+  return undefined
+})
 
 afterAll(() => {
-  // need to remove last element from users_data.json
-});
+  return undefined
+})
+
+/**
+ * If beforeEach is inside a describe block, it runs for each test in the describe block.
+ */
+
+beforeEach(() => {
+  return undefined
+})
+
+/**
+ * If afterEach is inside a describe block, it runs for each test in the describe block.
+ */
+
+afterEach(() => {
+  return undefined
+})
 
 describe('Testing class instances in users.js', () => {
-  const userInst = new usersM.User();
-  const usersInst = new usersM.Users();
+  const userInst = new usersM.User()
+  const usersInst = new usersM.Users()
 
   it('creates new User instance with valid parameters', () => {
-    expect(typeof userInst.username).toBe('string');
-    expect(typeof userInst.userScore).toBe('number');
-    expect(typeof userInst.currentStreak).toBe('number');
-    expect(typeof userInst.highestStreak).toBe('number');
-  });
+    expect(typeof userInst.username).toBe('string')
+    expect(typeof userInst.userScore).toBe('number')
+    expect(typeof userInst.currentStreak).toBe('number')
+    expect(typeof userInst.highestStreak).toBe('number')
+  })
 
   it('creates new Users instance with valid parameters', () => {
-    expect(typeof usersInst.fileName).toBe('string');
-    expect(typeof usersInst.userList).toBe('object');
-  });
-  
-  it('checks if the user was stored in the database', () => { 
-    usersInst.storeUser(userInst);
-    expect(usersInst.userList.user[usersInst.userList.user.length - 1].userData).toEqual(userInst.username);
-  });
+    expect(typeof usersInst.fileName).toBe('string')
+    expect(typeof usersInst.userList).toBe('object')
+  })
 
+  it('checks if the user was stored in the database', () => {
+    usersInst.storeUser(userInst)
+    expect(usersInst.userList.user[usersInst.userList.user.length - 1].userData).toEqual(userInst.username)
+  })
 })
